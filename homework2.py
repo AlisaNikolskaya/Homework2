4502fc7f6e788ef58fdf98d9df0abe2e367abc65
AlisaNikolskaya <nikolalisa13@gmail.com>
2024-05-31 16:52:52 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,4 +2,11 @@ number_of_completed_homeworks=12
 number_of_hours_spent=1.5
 сourse_name='Python'
 time_for_one_homework=number_of_hours_spent/number_of_completed_homeworks
-print('Курс:', сourse_name,',', 'всего задач:', number_of_completed_homeworks,',', 'затрачено часов:', number_of_hours_spent,',', 'среднее время выполнения:', time_for_one_homework)
+print('Курс:',сourse_name,',', 'всего задач:', number_of_completed_homeworks,',', 'затрачено часов:', number_of_hours_spent,',', 'среднее время выполнения:', time_for_one_homework)
+
+#вариант 2
+number_of_completed_homeworks=12
+number_of_hours_spent=1.5
+сourse_name='Python'
+time_for_one_homework=float(number_of_hours_spent/number_of_completed_homeworks)
+print('Курс: ' + str(сourse_name) + ', ' + 'всего задач: ' + str(number_of_completed_homeworks) + ', ' + 'затрачено часов: ' + str(number_of_hours_spent) + ', ' + 'среднее время выполнения: ' + str(time_for_one_homework))
